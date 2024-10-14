eb523da103a2b4250eb514d7b39301dc0731c06c
NolanCosta <nolan.costa@epitech.eu>
2024-10-14 15:24:55 +0200

    possibilité de créer ou rejoindre une entrprise

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Company;
+
+class CompanyController extends Controller
+{
+
+    public function index()
+    {
+        $companies = Company::all();
+        return response()->json($companies, 200);
+    }
+
+    public function store(Request $request)
+    {
+
+        if ($request->type === true) {
+            try {
+                $request->validate([
+                    'name' => 'required|string',
+                    'address' => 'required|string',
+                    'zipCode' => 'required|integer',
+                    'city' => 'required|string',
+                    'description' => 'required|string',
+                ]);
+
+                $existingCompany = Company::where('name', $request->name)->first();
+
+                if ($existingCompany) {
+                    return response()->json(['message' => 'Une entreprise avec ce nom existe déjà.'], 400);
+                }
+
+                $company = Company::create([
+                    'name' => $request->name,
+                    'address' => $request->address,
+                    'zip_code' => $request->zipCode,
+                    'city' => $request->city,
+                    'aboutUs' => $request->description,
+                    'user_id' => $request->user()->id,
+                ]);
+
+                $request->user()->update([
+                    'company_id' => $company->id
+                ]);
+
+                return response()->json(['message' => 'Companie créée avec succès'], 201);
+            } catch (\Exception $e) {
+                return response()->json(['message' => $e->getMessage()], 500);
+            }
+        } else {
+            try {
+                $request->validate([
+                    'company_id' => 'required|string',
+                ]);
+
+                $request->user()->update([
+                    'company_id' => $request->company_id
+                ]);
+
+                $company = Company::where('id', $request->company_id)->first();
+
+                if (!$company) {
+                    return response()->json(['message' => 'Companie non trouvée'], 404);
+                }
+
+                $collaborators = json_decode($company->collaborators, true) ?? [];
+                
+                if (!in_array($request->user()->id, $collaborators)) {
+                    $collaborators[] = $request->user()->id;
+                
+                    $company->update([
+                        'collaborators' => $collaborators
+                    ]);
+                } else {
+                    return response()->json(['message' => 'Vous avez déjà rejoint cette entreprise'], 400);
+                }
+
+                return response()->json(['message' => 'Vous avez rejoins ' . $company->name, 'company_id' => $company->id], 200);
+            } catch (\Exception $e) {
+                return response()->json(['message' => $e->getMessage()], 500);
+            }
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CompanyController;
 
 Route::post('user/register', [UserController::class, 'store']);
 Route::post('user/login', [UserController::class, 'auth']);
+Route::get('company', [CompanyController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', function (Request $request) {
@@ -16,4 +18,5 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::post('user/logout', [UserController::class, 'logout']);
+    Route::post('company/store' , [CompanyController::class, 'store']);
 });
