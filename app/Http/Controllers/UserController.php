7b4bffd768ec9ca08440d8b1cb3cbfed4cf0ecd1
Nolan <nolan.costa@epitech.eu>
2024-10-14 22:42:31 +0200

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,32 +7,6 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function store(Request $request)
-    {
-        try {
-            $request->validate([
-                'firstname' => 'required|string',
-                'lastname' => 'required|string',
-                'email' => 'required|email|unique:users',
-                'phone' => 'required|string',
-                'password' => 'required|string',
-            ]);
-
-            $user = User::create([
-                'firstname' => $request->firstname,
-                'lastname' => $request->lastname,
-                'email' => $request->email,
-                'phone' => $request->phone,
-                'password' => bcrypt($request->password),
-                'role' => 'USER',
-            ]);
-
-            return response()->json(['message' => 'Utilisateur créé avec succès', 201]);
-        } catch (\Exception $e) {
-            return response()->json(['message' => $e->getMessage()], 500);
-        }
-    }
-
     public function auth(Request $request)
     {
         $request->validate([
@@ -66,11 +40,37 @@ class UserController extends Controller
             return response()->json(['message' => $e->getMessage()], 500);
         }
     }
-
+    
     public function index()
     {
         $users = user::all();
         return $users;
+    }
+    
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'firstname' => 'required|string',
+                'lastname' => 'required|string',
+                'email' => 'required|email|unique:users',
+                'phone' => 'required|string',
+                'password' => 'required|string',
+            ]);
+
+            $user = User::create([
+                'firstname' => $request->firstname,
+                'lastname' => $request->lastname,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'password' => bcrypt($request->password),
+                'role' => 'USER',
+            ]);
+
+            return response()->json(['message' => 'Utilisateur créé avec succès', 201]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 
     public function update(Request $request, $id)
@@ -88,7 +88,4 @@ class UserController extends Controller
 
         return response()->json(['message' => 'User deleted successfully.'], 200);
     }
-
-
-   
 }
